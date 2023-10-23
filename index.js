84b77da982a33017383487eacbd4c66b20165b0f
Niamul-Hasan <zead.ece2014@gmail.com>
2023-10-23 13:41:10 +0600

    mongodb is connected

--- index.js
+++ index.js
@@ -11,6 +11,39 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ha0qjez.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    client.connect();
+   
+
+
+    app.get("/get",(req,res)=>{
+        res.send("MongoDB is well connected");
+    })
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello From GreenWay Academic Web!')
   })
